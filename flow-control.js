288272a0e5405c6ca28a2973f2dfcef178888030
Shahad Puthukudi <shahadp@gmail.com>
2020-05-02 11:21:44 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,8 +28,8 @@ function switchAge(age) {
   switch (age){
   case (age>=13 && age<=19):
     return "You are a teenager";
-    //break;
-    default:
+  break;
+  default:
     return "You have an age"
 }
 
